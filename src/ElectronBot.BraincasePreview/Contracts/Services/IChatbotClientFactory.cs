3d7bca62e3f4118d0301187ac88166055e6cd428
gil zhang <gil.zhang.dev@outlook.com>
2023-02-16 00:55:20 +0800

    添加对话机器人客户端工厂

---
+++ src/ElectronBot.BraincasePreview/Contracts/Services/IChatbotClientFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Contracts.Services;
+/// <summary>
+/// 聊天机器人工厂
+/// </summary>
+public interface IChatbotClientFactory
+{
+    /// <summary>
+    /// 创建聊天机器人客户端
+    /// </summary>
+    /// <param name="clientName"></param>
+    /// <returns></returns>
+    IChatbotClient CreateChatbotClient(string? clientName = null);
+}
---
+++ src/ElectronBot.BraincasePreview/Services/Chatbot/TuringChatbotClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Contracts.Services;
+
+namespace Services;
+public class TuringChatbotClient : IChatbotClient
+{
+    public string Name => "Turing";
+
+    public Task<string> AskQuestionResultAsync(string message) => throw new NotImplementedException();
+}
