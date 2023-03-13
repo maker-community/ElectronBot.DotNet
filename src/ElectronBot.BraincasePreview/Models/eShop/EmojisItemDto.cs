a5e7534f583033e7a2f5005fa18fd4d0cfe7f860
gil zhang <gil.zhang.dev@outlook.com>
2023-03-14 00:29:22 +0800

    添加表情商城对接

---
+++ src/ElectronBot.BraincasePreview/Models/eShop/EmojisItemDto.cs
@@ -0,0 +1,34 @@
+﻿namespace Models;
+public class EmojisItemDto
+{
+    public string Id
+    {
+        get; set;
+    } = string.Empty;
+
+    public string Name
+    {
+        get; set;
+    } = string.Empty;
+
+    public decimal Price
+    {
+        get; set;
+    }
+
+    public string PictureFileName
+    {
+        get;
+        set;
+    } = string.Empty;
+
+    public string PictureFileId
+    {
+        get; set;
+    } = string.Empty;
+
+    public string VideoFileId
+    {
+        get; set;
+    } = string.Empty;
+}
---
+++ src/ElectronBot.BraincasePreview/Services/eShop/EmojiseShopService.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Headers;
+using Contracts.Services;
+using ElectronBot.BraincasePreview.Models;
+using Models;
+
+namespace Services;
+public class EmojiseShopService : IEmojiseShopService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    private const string ProfileImageUploadUri = "";
+    public EmojiseShopService(IHttpClientFactory httpClientFactory)
+    {
+
+        _httpClientFactory = httpClientFactory;
+
+    }
+    public Task<EmoticonAction> DownloadEmojisAsync(string id) => throw new NotImplementedException();
+    public Task<EmojisItemDto> GetEmojisItemAsync(string id) => throw new NotImplementedException();
+    public Task<List<EmojisItemDto>> GetEmojisListAsync(EmojisItemQuery itemQuery) => throw new NotImplementedException();
+    public async Task<bool> UploadEmojisAsync(EmoticonAction emoticon)
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+
+        using (var content = new MultipartFormDataContent())
+        {
+            var mixed = new MultipartContent("mixed") 
+            {
+            //CreateFileContent (imageStream, "image.jpg", "image/jpeg"),
+            //CreateFileContent (signatureStream, "image.jpg.sig", "application/octet-stream")
+            };
+            content.Add(mixed, "files");
+            var response = await httpClient.PostAsync(ProfileImageUploadUri, content);
+            response.EnsureSuccessStatusCode();
+        }
+
+        return true;
+    }
+
+    private StreamContent CreateFileContent(Stream stream, string fileName, string contentType)
+    {
+        var fileContent = new StreamContent(stream);
+        fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("file") { FileName = fileName };
+        fileContent.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+        return fileContent;
+    }
+}
