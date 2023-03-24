d74c20204142e50d8b9b0388300b602c661d0fce
gil zhang <gil.zhang.dev@outlook.com>
2023-03-24 22:57:24 +0800

    封装动作滑动条控件

--- src/ElectronBot.BraincasePreview/Controls/ActionEdit.xaml.cs
+++ src/ElectronBot.BraincasePreview/Controls/ActionEdit.xaml.cs
@@ -20,15 +20,80 @@ using Windows.Foundation.Collections;
 namespace ElectronBot.BraincasePreview.Controls;
 public sealed partial class ActionEdit : UserControl
 {
-    public MainViewModel ViewModel
-    {
-        get;
-    }
-
     public ActionEdit()
     {
-        ViewModel = App.GetService<MainViewModel>();
-
         InitializeComponent();
+    }
+
+    public event RangeBaseValueChangedEventHandler ValueChanged;
+    public float J1
+    {
+        get => (float)GetValue(J1Property);
+        set => SetValue(J1Property, value);
+    }
+
+    // Using a DependencyProperty as the backing store for J1.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty J1Property =
+        DependencyProperty.Register("J1", typeof(float), typeof(ActionEdit), new PropertyMetadata(0));
+
+    public float J2
+    {
+        get => (float)GetValue(J2Property);
+        set => SetValue(J2Property, value);
+    }
+
+    // Using a DependencyProperty as the backing store for J2.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty J2Property =
+        DependencyProperty.Register("J2", typeof(float), typeof(ActionEdit), new PropertyMetadata(0));
+
+
+    public float J3
+    {
+        get => (float)GetValue(J3Property);
+        set => SetValue(J3Property, value);
+    }
+
+    // Using a DependencyProperty as the backing store for J3.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty J3Property =
+        DependencyProperty.Register("J3", typeof(float), typeof(ActionEdit), new PropertyMetadata(0));
+
+
+    public float J4
+    {
+        get => (float)GetValue(J4Property);
+        set => SetValue(J4Property, value);
+    }
+
+    // Using a DependencyProperty as the backing store for J4.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty J4Property =
+        DependencyProperty.Register("J4", typeof(float), typeof(ActionEdit), new PropertyMetadata(0));
+
+
+
+    public float J5
+    {
+        get => (float)GetValue(J5Property);
+        set => SetValue(J5Property, value);
+    }
+
+    // Using a DependencyProperty as the backing store for J5.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty J5Property =
+        DependencyProperty.Register("J5", typeof(float), typeof(ActionEdit), new PropertyMetadata(0));
+
+
+
+    public float J6
+    {
+        get => (float)GetValue(J6Property);
+        set => SetValue(J6Property, value);
+    }
+
+    // Using a DependencyProperty as the backing store for J6.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty J6Property =
+        DependencyProperty.Register("J6", typeof(float), typeof(ActionEdit), new PropertyMetadata(0));
+
+    private void Head_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+    {
+        ValueChanged?.Invoke(this, e);
     }
 }
