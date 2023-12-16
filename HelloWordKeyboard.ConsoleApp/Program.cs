f631fc43a5b2ec09f0276005fd233e48450e91d7
Gil Zhang <gil.zhang.dev@outlook.com>
2023-12-17 00:27:12 +0800

    add helloworldkeyboard proto file and test code

--- HelloWordKeyboard.ConsoleApp/Program.cs
+++ HelloWordKeyboard.ConsoleApp/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Google.Protobuf;
 using HidApi;
+using UsbComm;
+
+
 
 var device = new Device("\\\\?\\hid#vid_1d50&pid_615e&mi_01#8&a8a6dc9&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}"); //Fill vendor id and product id
 
@@ -7,24 +11,34 @@ Console.WriteLine(device.GetManufacturer());
 
 Console.WriteLine(device.GetDeviceInfo());
 
-device.SetNonBlocking(true);
+//device.SetNonBlocking(true);
 
-var buffer = new byte[64];
-buffer[0] = 0x01;
-buffer[1] = 62;
-buffer[2] = 8;
-buffer[3] = 1;
-buffer[4] = 18;
+var hidMesage = new UsbComm.MessageH2D
+{
+    Action = UsbComm.Action.Version
+};
+
+var bytes = hidMesage.ToByteArray();
+
+var listByte = new List<byte>();
+listByte.Add(1);
+listByte.Add((byte)(bytes.Length + 1));
+listByte.Add((byte)(bytes.Length));
+listByte.AddRange(bytes);
+
+
+listByte.AddRange(new byte[64 - listByte.Count]);
+
+var buffer = listByte.ToArray();
+
 device.Write(buffer);
+
+Task.Delay(20);
 var read = device.Read(64);
 
+var result = read[3..(read[2] + 3)];
 
-var buffer1 = new byte[64];
-buffer1[0] = 0x01;
-buffer1[1] = 62;
-buffer1[2] = 8;
-buffer1[3] = 1;
-buffer1[4] = 18;
-device.Write(buffer1);
-var read1 = device.Read(64);
+
+var data =  MessageD2H.Parser.ParseFrom(result);
+Console.WriteLine(data);
 Hid.Exit(); //Call at the end of your program
