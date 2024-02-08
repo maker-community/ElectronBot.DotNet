fa36037d57bb495d2529eeeedee509ba61c2b912
Gil Zhang <gil.zhang.dev@outlook.com>
2024-02-08 22:05:06 +0800

    add maui file

---
+++ src/Verdure.ElectronBot.Braincase.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Verdure.ElectronBot.Braincase.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ src/Verdure.ElectronBot.Braincase.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Verdure.ElectronBot.Braincase.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
