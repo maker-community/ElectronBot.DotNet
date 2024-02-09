c107ca63bad327a46af9166684358f38d0b8377f
Gil Zhang <gil.zhang.dev@outlook.com>
2024-02-09 11:54:45 +0800

    update project

--- src/Verdure.ElectronBot.Braincase.Maui/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Verdure.ElectronBot.Braincase.Maui;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
---
+++ src/Verdure.ElectronBot.Braincase.Maui/Pages/SettingPage1.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verdure.ElectronBot.Braincase.Maui.Pages;
+
+public partial class SettingPage1 : ContentPage
+{
+    public SettingPage1()
+    {
+        InitializeComponent();
+    }
+}
