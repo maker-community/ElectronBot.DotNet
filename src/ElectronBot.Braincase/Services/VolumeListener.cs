3d9768ab5ec1b04cbf8e3a71c7b6b86a9b11514a
Gil Zhang <gil.zhang.dev@outlook.com>
2024-05-09 15:52:13 +0800

    Update VolumeListener.cs

--- src/ElectronBot.Braincase/Services/VolumeListener.cs
+++ src/ElectronBot.Braincase/Services/VolumeListener.cs
@@ -6,35 +6,22 @@ namespace Services
 {
     public class VolumeListener
     {
-        private WasapiOut _output;
-        private MMDevice _device;
-
-        public event Action<AudioVolumeNotificationData> VolumeChanged;
-
-        public VolumeListener()
-        {
-            MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
-            _device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            _output = new WasapiOut(_device, AudioClientShareMode.Shared, true, 100);
-            _output.PlaybackStopped += OnPlaybackStopped;
-        }
-
-        public void StartListening()
-        {
-            _device.AudioEndpointVolume.OnVolumeNotification += OnVolumeNotification;
-            _output.Play();
-        }
-
-        private void OnPlaybackStopped(object sender, StoppedEventArgs e)
-        {
-            _output.Dispose();
-        }
-
-        private void OnVolumeNotification(AudioVolumeNotificationData data)
-        {
-            VolumeChanged?.Invoke(data);
-            // 这里是当音量改变时你想要执行的代码
-            Console.WriteLine("The volume has been changed to " + data.MasterVolume);
-        }
+     private MMDevice device;
+    
+     public event Action<AudioVolumeNotificationData> VolumeChanged;
+    
+     public VolumeListener()
+     {
+         MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
+         device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+         device.AudioEndpointVolume.OnVolumeNotification += OnVolumeNotification;
+     }
+    
+     private void OnVolumeNotification(AudioVolumeNotificationData data)
+     {
+         VolumeChanged?.Invoke(data);
+         // 这里是当音量改变时你想要执行的代码
+         Console.WriteLine("The volume has been changed to " + data.MasterVolume);
+     }
     }
 }
