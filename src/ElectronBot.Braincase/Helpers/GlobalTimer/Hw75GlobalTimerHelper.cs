9c7b314a097c9843adc2c301904d7f2aed043256
Gil Zhang <gil.zhang.dev@outlook.com>
2024-08-26 01:35:56 +0800

    add hw75 font

--- src/ElectronBot.Braincase/Helpers/GlobalTimer/Hw75GlobalTimerHelper.cs
+++ src/ElectronBot.Braincase/Helpers/GlobalTimer/Hw75GlobalTimerHelper.cs
@@ -1,12 +1,18 @@
-﻿using ElectronBot.Braincase;
+﻿using System.Numerics;
+using ElectronBot.Braincase;
 using ElectronBot.Braincase.Contracts.Services;
 using ElectronBot.Braincase.Helpers;
 using ElectronBot.Braincase.Models;
 using HelloWordKeyboard.DotNet;
 using Microsoft.UI.Xaml;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
-using Windows.Storage.Streams;
+using Windows.Storage;
+using HorizontalAlignment = SixLabors.Fonts.HorizontalAlignment;
+using VerticalAlignment = SixLabors.Fonts.VerticalAlignment;
 
 namespace Helpers;
 public class Hw75GlobalTimerHelper
@@ -18,27 +24,39 @@ public class Hw75GlobalTimerHelper
 
     public Hw75GlobalTimerHelper()
     {
-        // Set the interval to 1 second
-        timer.Interval = TimeSpan.FromSeconds(60);
-
-        // Set the event handler for the Tick event
+        timer.Interval = TimeSpan.FromSeconds(6);
         timer.Tick += Timer_Tick;
     }
 
     private void Timer_Tick(object? sender, object e)
     {
-        App.MainWindow?.DispatcherQueue.TryEnqueue(async () =>
+        App.MainWindow.DispatcherQueue.TryEnqueue(async () =>
         {
             try
             {
                 var _localSettingsService = App.GetService<ILocalSettingsService>();
-                var config = await _localSettingsService.ReadSettingAsync<CustomClockTitleConfig>(Constants.CustomClockTitleConfigKey);
-                using var image = SixLabors.ImageSharp.Image.Load<Rgba32>(config?.CustomHw75ImagePath ?? "");
+                var config = await _localSettingsService.ReadSettingAsync<CustomClockTitleConfig>(Constants.CustomClockTitleConfigKey) ?? new CustomClockTitleConfig();
+
+                using var image = await LoadImageAsync(config.CustomHw75ImagePath);
+
+                var font = await GetFontAsync(config.Hw75CustomContentFontSize);
+
+                // 计算文本尺寸
+                TextOptions textOptions = new TextOptions(font)
+                {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center
+                };
+                var textSize = TextMeasurer.MeasureSize(config.Hw75CustomContent, textOptions);
+
+                // 计算文本居中位置
+                PointF center = new PointF(128 / 2, 296 / 2);
+
 
                 image.Mutate(x =>
                 {
                     x.Resize(128, 296);
-                    //x.Grayscale();
+                    x.DrawText(config.Hw75CustomContent, font, Color.Black, new Vector2(textSize.X, textSize.Y));
                 });
                 var byteArray = image.EnCodeImageToBytes();
 
@@ -52,14 +70,41 @@ public class Hw75GlobalTimerHelper
 
     public void StartTimer()
     {
-
-        // Start the timer
         timer.Start();
     }
 
     public void StopTimer()
     {
-        // Stop the timer when the application closes
         timer.Stop();
+    }
+
+    private async Task<Font> GetFontAsync(float size, string fontName = "fusion-pixel-12px-monospaced-zh_hans.ttf")
+    {
+        var collection = new FontCollection();
+
+        using var stream = await GetFileStreamAsync($"ms-appx:///Assets/Font/{fontName}");
+        var family = collection.Add(stream);
+        var font = family.CreateFont(size, FontStyle.Bold);
+        return font;
+    }
+
+    private async Task<Image<Rgba32>> LoadImageAsync(string imagePath)
+    {
+        if (imagePath.StartsWith("ms-appx"))
+        {
+            using var stream = await GetFileStreamAsync(imagePath);
+            return Image.Load<Rgba32>(stream);
+        }
+        else
+        {
+            return Image.Load<Rgba32>(imagePath);
+        }
+    }
+
+    private async Task<Stream> GetFileStreamAsync(string filePath)
+    {
+        var storageFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri(filePath));
+        var randomAccessStream = await storageFile.OpenAsync(FileAccessMode.Read);
+        return randomAccessStream.AsStreamForRead();
     }
 }
