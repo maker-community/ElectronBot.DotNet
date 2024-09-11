e55ba8dd9d5aa4340c883600be67fc34b417dabe
gil zhang <gil.zhang.dev@outlook.com>
2024-09-12 00:40:38 +0800

    update groove view

--- src/ElectronBot.Braincase/ClockViews/GrooveClockView.xaml.cs
+++ src/ElectronBot.Braincase/ClockViews/GrooveClockView.xaml.cs
@@ -14,6 +14,7 @@ using Microsoft.UI.Xaml.Hosting;
 using Microsoft.UI.Xaml.Input;
 using Windows.UI;
 using Windows.Foundation;
+using Services;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,18 +22,6 @@ using Windows.Foundation;
 namespace ElectronBot.Braincase.ClockViews;
 public sealed partial class GrooveClockView : UserControl
 {
-
-    const float defaultDpi = 96;
-    CanvasRenderTarget glassSurface;
-    CanvasBitmap imgbackground;
-    GaussianBlurEffect blurEffect;
-    RainyDay rainday;
-    float scalefactor;
-    float imgW;
-    float imgH;
-    float imgX;
-    float imgY;
-
     public ClockViewModel ViewModel
     {
         get;
@@ -42,79 +31,9 @@ public sealed partial class GrooveClockView : UserControl
     {
         this.InitializeComponent();
         ViewModel = App.GetService<ClockViewModel>();
-    }
+        var spectrumAnalyzer = new SpectrumAnalyzer(canvas);
 
-    private async void Canvas_CreateResources(CanvasControl sender, CanvasCreateResourcesEventArgs args)
-    {
-        await PrepareRaindayAsync(sender);
-    }
-
-    private async Task PrepareRaindayAsync(CanvasControl sender)
-    {
-        var imgPath = Path.Combine(AppContext.BaseDirectory, $"Assets/Images/CustomViewDefault.jpg");
-
-        var blurAmount = 4.0f;
-
-        if (ViewModel.BotSetting != null)
-        {
-            if (!string.IsNullOrEmpty(ViewModel.BotSetting.CustomViewPicturePath))
-            {
-                imgPath = ViewModel.BotSetting.CustomViewPicturePath;
-            }
-
-            blurAmount = ViewModel.BotSetting.GaussianBlurValue;
-
-            if (!ViewModel.BotSetting.CustomViewContentIsVisibility)
-            {
-                PomodoroPanel.Visibility = Visibility.Collapsed;
-            }
-        }
-
-        imgbackground = await CanvasBitmap.LoadAsync(sender, imgPath);
-
-        blurEffect = new GaussianBlurEffect()
-        {
-            Source = imgbackground,
-            BlurAmount = blurAmount,
-            BorderMode = EffectBorderMode.Soft
-        };
-        scalefactor =  (float)Math.Min(sender.Size.Width / imgbackground.Size.Width, sender.Size.Height / imgbackground.Size.Height);
-        imgW = (float)imgbackground.Size.Width * scalefactor;
-        imgH = (float)imgbackground.Size.Height * scalefactor;
-        imgX = (float)(sender.Size.Width - imgW) / 2;
-        imgY = (float)(sender.Size.Height - imgH) / 2;
-        glassSurface = new CanvasRenderTarget(sender, imgW, imgH, defaultDpi);
-
-        List<List<float>> pesets;
-
-
-        rainday = new RainyDay(sender, imgW, imgH, imgbackground)
-        {
-            ImgSclaeFactor = scalefactor,
-            GravityAngle = (float)Math.PI / 2
-        };
-        pesets = new List<List<float>>() {
-
-            new List<float> { 3, 3, 0.88f },
-            new List<float> { 5, 5, 0.9f },
-            new List<float> { 6, 2, 1 }
-        };
-
-        rainday.Rain(pesets, 100);
-    }
-
-    private void Canvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
-    {
-        if (imgbackground != null)
-        {
-            args.DrawingSession.DrawImage(blurEffect, new Rect(imgX, imgY, imgW, imgH), new Rect(0, 0, imgbackground.Size.Width, imgbackground.Size.Height));
-            args.DrawingSession.DrawImage(glassSurface, imgX, imgY);
-
-            using var ds = glassSurface.CreateDrawingSession();
-            rainday.UpdateDrops(ds);
-
-        }
-        canvas.Invalidate();
+        spectrumAnalyzer.Start();
     }
 
     private void UserControl_Unloaded(object sender, RoutedEventArgs e)
