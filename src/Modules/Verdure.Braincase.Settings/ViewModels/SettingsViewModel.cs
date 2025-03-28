2f20489c07dc4d7429f1f5989df6f387eb20d9fd
gil zhang <gil.zhang.dev@outlook.com>
2025-03-29 01:30:41 +0800

    update setting page

--- src/Modules/Verdure.Braincase.Settings/ViewModels/SettingsViewModel.cs
+++ src/Modules/Verdure.Braincase.Settings/ViewModels/SettingsViewModel.cs
@@ -154,7 +154,7 @@ public partial class SettingsViewModel : ObservableRecipient
             ClockTitleConfig.Hw75CustomContentIsVisibility = isVisual;
         }
 
-        await _localSettingsService.SaveSettingAsync(Constants.CustomClockTitleConfigKey, ClockTitleConfig);
+        //await _localSettingsService.SaveSettingAsync(Constants.CustomClockTitleConfigKey, ClockTitleConfig);
     }
 
     public async void Hw75TimeToggleSwitch_OnToggled(object sender, RoutedEventArgs e)
@@ -165,7 +165,7 @@ public partial class SettingsViewModel : ObservableRecipient
             ClockTitleConfig.Hw75TimeIsVisibility = isVisual;
         }
 
-        await _localSettingsService.SaveSettingAsync(Constants.CustomClockTitleConfigKey, ClockTitleConfig);
+        //await _localSettingsService.SaveSettingAsync(Constants.CustomClockTitleConfigKey, ClockTitleConfig);
     }
 
     public async void Hw75ToggleSwitch_OnToggled(object sender, RoutedEventArgs e)
@@ -176,7 +176,7 @@ public partial class SettingsViewModel : ObservableRecipient
             ClockTitleConfig.Hw75IsOpen = isVisual;
         }
 
-        await _localSettingsService.SaveSettingAsync(Constants.CustomClockTitleConfigKey, ClockTitleConfig);
+        //await _localSettingsService.SaveSettingAsync(Constants.CustomClockTitleConfigKey, ClockTitleConfig);
     }
 
     public async void RangeBase_OnValueChanged(object sender, RangeBaseValueChangedEventArgs e)
@@ -293,6 +293,7 @@ public partial class SettingsViewModel : ObservableRecipient
     private async Task TextChanged()
     {
         await _localSettingsService.SaveSettingAsync(Constants.CustomClockTitleConfigKey, ClockTitleConfig);
+        ToastHelper.SendToast("Save Ok", TimeSpan.FromSeconds(3));
     }
 
     /// <summary>
